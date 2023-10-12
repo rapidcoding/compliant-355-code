8572e94fbfaea9ce6e861f7485ee607702bab4bb
rapidcoding <145047912+rapidcoding@users.noreply.github.com>
2023-10-12 14:39:12 -0500

    Update STR52-CPP.cpp

--- STR52-CPP/STR52-CPP.cpp
+++ STR52-CPP/STR52-CPP.cpp
@@ -1,25 +1,25 @@
 #include <string>
 #include <iostream>
 
-// Function to convert all semi colons to space in a text such as an email
+// Function to convert all spaces to underscores in a text such as an email
 // This safely follows the rule STR52-CPP as we are using a range based loop which avoids explicit iterators or references to characters
 void function(const std::string &input)
 {
     std::string email;
 
-    std::cout << input << " is the example email with semi colons\n";
+    std::cout << input << " is the example email with spaces\n";
 
     for (char c : input)
     {
-        email.push_back(c != ';' ? c : ' ');
+        email.push_back(c != ' ' ? c : '_');
     }
 
-    std::cout << email << " is the example email after safely removing the semi colons\n";
+    std::cout << email << " is the example email after safely removing the spaces\n";
 }
 
 int main()
 {
-    std::string text = "example;email;with;semicolons";
+    std::string text = "example email test@gmail.com";
     function(text);
     return 0;
 }
